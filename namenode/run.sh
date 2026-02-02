232f741977bcf6203b76ac11221b02a6a1d57afc
umess-ss <ums.rbc07@gmail.com>
2026-02-02 13:38:32 -0500

    updated code

--- namenode/run.sh
+++ namenode/run.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-namedir=`echo $HDFS_CONF_dfs_namenode_name_dir | perl -pe 's#file://##'`
-if [ ! -d $namedir ]; then
-  echo "Namenode name directory not found: $namedir"
-  exit 2
+# Wait for configuration to be available
+if [ ! -f ${HADOOP_CONF_DIR}/core-site.xml ]; then
+    echo "Waiting for Hadoop configuration..."
+    sleep 5
 fi
 
-if [ -z "$CLUSTER_NAME" ]; then
-  echo "Cluster name not specified"
-  exit 2
+# Format NameNode if it hasn't been formatted yet
+if [ ! -d /hadoop/dfs/name/current ]; then
+    echo "Formatting NameNode..."
+    ${HADOOP_HOME}/bin/hdfs namenode -format -force -nonInteractive
 fi
 
-echo "remove lost+found from $namedir"
-rm -r $namedir/lost+found
-
-if [ "`ls -A $namedir`" == "" ]; then
-  echo "Formatting namenode name directory: $namedir"
-  $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
-fi
-
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
+# Start NameNode
+echo "Starting NameNode..."
+${HADOOP_HOME}/bin/hdfs namenode
